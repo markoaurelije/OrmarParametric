ddfeb706354f9d6f569d8df2ebcd40e312d839c3
Marko V <markoaurelije@gmail.com>
2025-02-24 11:57:24 +0100

    added kuhinja dolje preset

--- commands/commandDialog/presets.py
+++ commands/commandDialog/presets.py
@@ -101,7 +101,111 @@ kuhinja_viseci_element = [
         "expression": "2",
     },
 ]
+kuhinja_donji_element = [
+    {
+        "paramName": "J1_sirina",
+        "expression": "60.00 cm",
+    },
+    {
+        "paramName": "J1_dubina",
+        "expression": "60.0 cm",
+    },
+    {
+        "paramName": "J1_visina",
+        "expression": "78.0 cm",
+    },
+    {
+        "paramName": "J1_donja_ploca_debljina",
+        "expression": "18.0 mm",
+    },
+    {
+        "paramName": "J1_bokovi_na_donju_plocu",
+        "expression": "1",
+    },
+    {
+        "paramName": "J1_bokovi_do_gornje_ploce",
+        "expression": "0",
+    },
+    {
+        "paramName": "J1_bok_lijevo_debljina",
+        "expression": "18.0 mm",
+    },
+    {
+        "paramName": "J1_bok_desno_debljina",
+        "expression": "18.0 mm",
+    },
+    {
+        "paramName": "J1_gornja_ploca",
+        "expression": "0",
+    },
+    # {
+    #     "paramName": "J1_gornja_ploca_debljina",
+    #     "expression": "18.0 mm",
+    # },
+    # {
+    #     "paramName": "J1_gornja_napust",
+    #     "expression": "0.0 mm",
+    # },
+    {
+        "paramName": "J1_ukrute",
+        "expression": "1",
+    },
+    {
+        "paramName": "J1_ukruta_širina",
+        "expression": "80.0 mm",
+    },
+    {
+        "paramName": "J1_leđa_debljina",
+        "expression": "3.0 mm",
+    },
+    {
+        "paramName": "J1_leđa_ofset",
+        "expression": "1.0 mm",
+    },
+    {"paramName": "J1_leđa_upust", "expression": "0 mm"},
+    {
+        "paramName": "J1_leđa_dubina_slota_u_bokovima",
+        "expression": "0 mm",
+    },
+    {
+        "paramName": "J1_fronta",
+        "expression": "1",
+    },
+    {
+        "paramName": "J1_fronta_pokriva_donju_plocu",
+        "expression": "1",
+    },
+    {
+        "paramName": "J1_fronta_unutarnje_pokrivanje",
+        "expression": "0",
+    },
+    {
+        "paramName": "J1_fronta_ofset",
+        "expression": "2.0 mm",
+    },
+    {
+        "paramName": "J1_fronta_debljina",
+        "expression": "19.0 mm",
+    },
+    # {
+    #     "paramName": "J1_fronta_pokriva_gornju_plocu",
+    #     "expression": "1",
+    # },
+    {
+        "paramName": "J1_polica_upust",
+        "expression": "20.0 mm",
+    },
+    {
+        "paramName": "J1_polica_suzenje",
+        "expression": "1.0 mm",
+    },
+    {
+        "paramName": "J1_broj_polica",
+        "expression": "2",
+    },
+]
 
 presets = {
     "kuhinja_viseci_element": kuhinja_viseci_element,
+    "kuhinja_donji_element": kuhinja_donji_element,
 }
